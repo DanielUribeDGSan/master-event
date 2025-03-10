a0918d865350337ba0a003d6fde715a6d994b344
DanielUribeDGSan <daniel.uribe.garcia07@gmail.com>
2025-03-10 14:28:42 -0600

    feat: new changes

--- src/routers/PrivateRoutes.tsx
+++ src/routers/PrivateRoutes.tsx
@@ -1,13 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Live } from "../screens/live/Live";
-import { Home } from "../screens/home/Home";
+
 import { Agenda } from "../screens/agenda/Agenda";
 import { Ponentes } from "../screens/ponentes/Ponentes";
 
 export const PrivateRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Live />} />
       <Route path="/agenda" element={<Agenda />} />
       <Route path="/ponentes" element={<Ponentes />} />
       <Route path="/en-vivo" element={<Live />} />
